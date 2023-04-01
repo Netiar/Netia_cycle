d620a4d0ba39fbbc56fcddf505a291cac1103a72
Netiar <collimow54@gmail.com>
2023-04-01 03:02:54 +0300

    add file

--- app/src/main/java/com/example/netia_cycle/models/Netia.java
+++ app/src/main/java/com/example/netia_cycle/models/Netia.java
@@ -1,4 +1,17 @@
 package com.example.netia_cycle.models;
 
+import android.os.SystemClock;
+
 public class Netia {
+    public final long timestamp = SystemClock.elapsedRealtime();
+    public final String message;
+    public final int activityHash;
+    public final int viewHash;
+
+    public Netia(String message, int activityHash, int viewHash) {
+        this.message = message;
+        this.activityHash = activityHash;
+        this.viewHash = viewHash;
+    }
+
 }
