68dc6f2f7769c503ec0a0a16122450ce964923b8
Netiar <collimow54@gmail.com>
2023-04-01 03:01:44 +0300

    rf:refactor NetiaViewHolder constructor

--- app/src/main/java/com/example/netia_cycle/adapters/NetiaViewHolder.java
+++ app/src/main/java/com/example/netia_cycle/adapters/NetiaViewHolder.java
@@ -1,4 +1,33 @@
 package com.example.netia_cycle.adapters;
 
-public class NetiaviewHolder {
+import android.text.format.DateUtils;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.netia_cycle.databinding.NetiaBinding;
+import com.example.netia_cycle.models.Netia;
+
+public class NetiaViewHolder extends RecyclerView.ViewHolder {
+    private final long startTime;
+    private final NetiaBinding binding;
+
+
+    public NetiaViewHolder(NetiaBinding binding, long startTime) {
+        super(binding.getRoot());
+        this.startTime = startTime;
+        this.binding = binding;
+    }
+
+
+    public void bindTo(Netia netia) {
+        long elapsedSeconds = (netia.timestamp - startTime)/1000;
+
+        binding.timestamp.setText(DateUtils.formatElapsedTime(elapsedSeconds));
+        binding.message.setText(netia.message);
+        binding.activityHash.setText(Integer.toHexString(netia.activityHash));
+        binding.viewHash.setText(Integer.toHexString(netia.viewHash));
+    }
+
+
+
 }
