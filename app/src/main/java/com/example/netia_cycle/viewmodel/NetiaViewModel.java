0a0292da06bb63f853820d735da6294447ab1a3d
Netiar <collimow54@gmail.com>
2023-04-01 03:04:25 +0300

    add NetiaViewModel file

--- app/src/main/java/com/example/netia_cycle/viewmodel/NetiaViewModel.java
+++ app/src/main/java/com/example/netia_cycle/viewmodel/NetiaViewModel.java
@@ -1,4 +1,27 @@
 package com.example.netia_cycle.viewmodel;
 
-public class NetiaViewModel {
+import android.os.SystemClock;
+
+import androidx.lifecycle.ViewModel;
+
+import com.example.netia_cycle.models.Netia;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class NetiaViewModel extends ViewModel {
+    public final List<Netia> netia = new ArrayList<>();
+    public final long startTime = SystemClock.elapsedRealtime();
+    private final int id = new Random().nextInt();
+
+    public void addEvent(String message, int activityHash) {
+        netia.add(new Netia(message, activityHash, id));
+    }
+
+    @Override
+    protected void onCleared() {
+        netia.clear();
+    }
+
 }
