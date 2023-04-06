28169cfb98bc37444d7650ea4a43223381115826
Netiar <collimow54@gmail.com>
2023-04-06 20:56:56 +0300

    rf: refactor class to implement Parcelable

--- app/src/main/java/com/example/netia_cycle/models/Netia.java
+++ app/src/main/java/com/example/netia_cycle/models/Netia.java
@@ -1,9 +1,13 @@
 package com.example.netia_cycle.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.os.SystemClock;
 
-public class Netia {
-    public final long timestamp = SystemClock.elapsedRealtime();
+import androidx.annotation.NonNull;
+
+public class Netia implements Parcelable {
+    public final long timestamp;
     public final String message;
     public final int activityHash;
     public final int viewHash;
@@ -12,6 +16,40 @@ public class Netia {
         this.message = message;
         this.activityHash = activityHash;
         this.viewHash = viewHash;
+        this.timestamp = SystemClock.elapsedRealtime();
     }
 
+
+    protected Netia(Parcel in) {
+        timestamp = in.readLong();
+        message = in.readString();
+        activityHash = in.readInt();
+        viewHash = in.readInt();
+    }
+
+    public static final Creator<Netia> CREATOR = new Creator<Netia>() {
+        @Override
+        public Netia createFromParcel(Parcel in) {
+            return new Netia(in);
+        }
+
+        @Override
+        public Netia[] newArray(int size) {
+            return new Netia[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeLong(timestamp);
+        dest.writeString(message);
+        dest.writeInt(activityHash);
+        dest.writeInt(viewHash);
+    }
 }
